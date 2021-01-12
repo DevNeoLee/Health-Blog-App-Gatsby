f7e3d43a1f4809c998fb01994b1049854d0f2f34
DevNeoLee <devneolee@gmail.com>
2021-01-12 11:44:54 -0800

    Change Footer CSS margin

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -19,34 +20,22 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header prop={'hello'} siteTitle={data.site.siteMetadata?.title || `Title`}
-               />
+    <React.Fragment className="fragment">
+      <Header prop={'hello'} siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          // padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-         
-          © {new Date().getFullYear()}, Built by <Link to='/'>건강백세 프로젝트</Link>
-          {` `} 
-          {data.site.siteMetadata?.description || `Description`}
-          <Link to="/">홈페이지로 돌아가기</Link>
-          <p>Photo by Ella Olsson from Pexels</p>
-        </footer>
       </div>
-    </>
+      <Footer data ={ data } />
+    </React.Fragment>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+
 
 export default Layout
