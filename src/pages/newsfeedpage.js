3f30e771aeb9e5b806df277182cc596cb5edd5e7
DevNeoLee <devneolee@gmail.com>
2021-01-12 13:38:24 -0800

    Set up contentful config

--- src/pages/newsfeedpage.js
+++ src/pages/newsfeedpage.js
@@ -1,28 +1,10 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import NewsFeed from '../components/newsfeed'
 
 const NewsFeedPage = () => {
-  const data = useStaticQuery(graphql`
-    query  {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              title,
-              date
-            }
-          fields {
-            slug
-            }
-          }
-        }
-      }
-    }
-  `)
 
   return (<Layout>
     <SEO title="건강뉴스 모음" />
